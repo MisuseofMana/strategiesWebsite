87350afc9aec77f17d8051b5d13b695d9f8cd4c9
Sean Yager <contact@seanyager.com>
2020-09-24 17:06:58 -0500

    Created new components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://thestrategies.com">
+      <img src="../assets/imgs/logos/theStrat.png">
+    </a>
+
+    <a role="button" @click="toggleNav()" class="navbar-burger burger" :class="{'is-active': navBarToggle}" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <br>
+
+  <div id="navbarBasicExample" :class="{'is-active': navBarToggle}" class="navbar-menu">
+    <div class="navbar-start">
+        <router-link class="navbar-item" to="/" exact><span class="icon"><i class="mdi mdi-newspaper-variant-outline"></i></span> The Strategies</router-link>
+        <router-link class="navbar-item" to="/RehabStrategies"><span class="icon"><i class="mdi mdi-handshake-outline"></i></span> Rehab Strategies</router-link>
+        <router-link class="navbar-item" to="/StaffingStrategies"><span class="icon"><i class="mdi mdi-account-group"></i></span> Staffing Strategies</router-link>
+        <router-link class="navbar-item" to="/CareStrategies"><span class="icon"><i class="mdi mdi-bottle-tonic-plus-outline"></i></span> Care Strategies</router-link>
+        <router-link class="navbar-item" to="/InnovativeTreatmentStrategies"><span class="icon"><i class="mdi mdi-desktop-classic"></i></span> Innovative Treatment Strategies</router-link>
+        <router-link class="navbar-item" to="/OurTeam"><span class="icon"><i class="mdi mdi-briefcase-outline"></i></span> Our Team</router-link>
+    </div>
+  </div>
+    <br><br>
+</nav>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+        data() {
+          return {
+            navBarToggle: false,
+          }
+        },
+        methods: {
+          toggleNav() {
+            this.navBarToggle = !this.navBarToggle;
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/StaffingStrategies.vue
+++ src/components/StaffingStrategies.vue
@@ -1,96 +1,106 @@
 <template>
-<div>
-      <el-main>
+  <div>
+        <div class="block flexCenter">
+        <img class="logoConstraint" src="../assets/imgs/logos/staffing.png"/>
+    </div>
 
-        <el-row :gutter="40">
-            <el-col :span="24">
-              <h1>Staffing Strategies</h1>
-            </el-col>
-        </el-row>
+    <hr>
 
-          <el-row :gutter="40">
-            <el-col :span="12">
-
-                <el-image
-                    :src="cityUrl"
-                    fit="fill">
-                  </el-image>
-
-                  <h2>We are "Staffing Strategies"</h2>
-
-                  <p>Staffing Strategies was born out of a need for supplemental staff for our sister company, Rehab
-Strategies. We first established a solid therapy/nursing pool with a reputation for quality and reliability.
-We then began selectively offering our services to other facilities with the same high-quality standards.
-Our promise to our client is that we would never place a therapist or nurse in their facility that we would
-not place in our own Rehab Strategies or Care Strategies facilities.</p>
-            </el-col>
-
-            <el-col :span="12">
-                <h1>
-                  We hire, we work, we share our staff
-                </h1>
-
-                <h2>Are You Looking for a Hero?</h2>
-
-                <p>Our partners contact us when they need to supplement staff. We develop close relationships with our
-client partners and tailor our services to their immediate needs. Consider us your staffing RESCUE
-SQUAD – we are here when you need us - for as little as two hours up to 13-week assignments. In
-addition to providing additional staff we provide RELIEF and peace of mind.
-As a Staffing Strategies client partner, you will have access to dependable, professional, and fully vetted
-staff. All nursing and therapy staff are subjected to the following:</p>
-
-                <ul>
-                  <li>Pre-Employment Background Screen</li>
-                  <li>Pre-Employment Drug Screen</li>
-                  <li>TB Test</li>
-                  <li>License Verification</li>
-                  <li>Monthly OIG Sanction Checks</li>
-                  <li>CPR Card</li>
-                  <li>Job Description</li>
-                  <li>Hep B Acknowledgement</li>
-                  <li>Flu Acknowledgement</li>
-                  <li>Confidentiality Agreement</li>
-                </ul>
-
-
-                <h2>Click below for to schedule a meeting with us.</h2>
-
-<el-button type="primary">
-  <i class="el-icon-s-order"></i>
-  Meet With our Staffing Coordinator</el-button>
-
-
-            </el-col>
-          </el-row>
-        </el-main>
-
-         <el-footer height="300px;">
-          <div>
-            <h2>Get In Touch</h2>
-              <p>Our Address Here</p>
-              <p>Our Address Here</p>
-              <p>Phone Number Here</p>
-              <p>Email Address</p>
-          </div>
-          <div>
-            <h2>CONTACT INFO</h2>
-            <p>Phone Number Here</p>
-            <p>Toll Free Number</p>
-            <p>Fax</p>
-            <p>Email Address</p>
-          </div>
-          <div>
-            <h2>Photography</h2>
-            <ul>
-              <li>
-<span><a href="https://unsplash.com/@samsonyyc?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Samson</a> on <a href="https://unsplash.com/@samsonyyc?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>                </li>
-            </ul>
-          </div>
-
-        </el-footer>
+    <div class="box">
+    <div class="columns">
+        <div class="column">
+            <div class="container">
+                <div class="content">
+                    <h1 class="title">Who Are We?</h1>
+                    <p>
+                        Staffing Strategies was born out of a need for supplemental staff for our sister company, Rehab Strategies. We first established a solid therapy/nursing pool with a reputation for quality and reliability. We then began selectively offering our services to other facilities with the same high-quality standards. Our promise to our client is that we would never place a therapist or nurse in their facility that we would not place in our own Rehab Strategies or Care Strategies facilities.                    
+                    </p>
+                    
+                
+                </div>
+            </div>
         </div>
+    </div>
+    </div>
 
+
+<div class="box">
+        <div class="columns is-variable is-8">
+            <div class="level">
+                <div class="column">
+                  <div class="content flexCenter">
+                    <h1 class="title">
+                          We hire, we work, we share our staff.
+                        </h1>
+                  </div>
+                    <img src="../assets/imgs/city.jpg"/>
+                </div>
+                <div class="column ">
+                    <div class="content">
+
+                      <h2 class="subtitle">Are You Looking for a Hero?</h2>
+
+                        <button class="button is-primary"><span class="icon"><i class="mdi mdi-calendar"></i></span><span>Schedule with our Staffing Coordinator</span></button>
+
+                        <br><br>
+
+                        <p>
+                            Our partners contact us when they need to supplement staff. We develop close relationships with our client partners and tailor our services to their immediate needs. Consider us your staffing RESCUE SQUAD – we are here when you need us - for as little as two hours up to 13-week assignments. In addition to providing additional staff we provide RELIEF and peace of mind. As a Staffing Strategies client partner, you will have access to dependable, professional, and fully vetted staff. All nursing and therapy staff are subjected to the following:
+                        </p>
+
+                        <ul>
+                            <li>Pre-Employment Background Screen</li>
+                            <li>Pre-Employment Drug Screen</li>
+                            <li>TB Test</li>
+                            <li>License Verification</li>
+                            <li>Monthly OIG Sanction Checks</li>
+                            <li>CPR Card</li>
+                            <li>Job Description</li>
+                            <li>Hep B Acknowledgement</li>
+                            <li>Flu Acknowledgement</li>
+                            <li>Confidentiality Agreement</li>
+                        </ul>
+                </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+    <div class="content has-text-centered">
+        <div class="columns">
+            <div class="column">
+                <div class="content">
+                    <h2>Find Us</h2>
+              <p>Our Address Here</p>
+              <p>Our Address Here</p>
+              <p>Our Address Here</p>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="content">
+                    <h2>Contact Us</h2>
+                    <p>Phone Number Here</p>
+                    <p>Toll Free Number</p>
+                    <p>Fax</p>
+                    <p>Email Address</p>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="content">
+                    <h2>Photography</h2>
+<span><a href="https://unsplash.com/@samsonyyc?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Samson</a> on <a href="https://unsplash.com/@samsonyyc?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></div>
+            </div>
+        </div>
+    </div>
+    </footer>
+  </div>
 </template>
+
+
 
 <script>
     export default {
